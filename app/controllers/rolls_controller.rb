f041f02d8b5729ed231163f1d680ca8401354aa2
lprates05 <lorenapratesdl@outlook.com>
2025-04-21 21:07:31 +0000

    dice_dynamic

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -5,7 +5,18 @@ class RollsController < ApplicationController
   end
 
   def combination
-    render({:template => "rolls_template/rolls_dice"})
+    
+
+      @num_dice = params.fetch("rolls").to_i
+      @dice_sides = params.fetch("dice").to_i
+      @rolls = []
+      @num_dice.times do
+        die = rand (1..@dice_sides)
+    
+        @rolls.push(die)
+      end
+      
+      render({:template => "rolls_template/dice_roll"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/", {:controller => "rolls", :action => "homepage"})
 
   get("/dice/:rolls/:dice", {:controller => "rolls", :action => "combination"})
+
+ 
 end
